405e2e281efa6a6616eb489d6d66dd7806530f62
Maria Josefsson <79754751+m4ryjo@users.noreply.github.com>
2022-03-29 14:23:10 +0200

    Update GcompBART.R

--- GcompBART/R/GcompBART.R
+++ GcompBART/R/GcompBART.R
@@ -1,13 +1,12 @@
 GcompBART <- function(data,
                       var.type,
-                      exp.reg,
-                      cont.reg,
+                      regimes, # list of regimes to compare
                       J=2000,
                       Ndraws=200,
                       Suppress = TRUE,
                       By = Ndraws/10,
                       ...
-                      ) {
+) {
 
   ## test if continuous or binary
   continous <- apply(data, 2, function(x) !all(x %in% 0:1))
@@ -24,17 +23,17 @@ GcompBART <- function(data,
     if(var.type[1 + i] == "Fi") {
       BModels[[i]] <- NA
     } else if (continous[1+i] == FALSE & var.type[1 + i] != "Fi" & Suppress == TRUE) {
-      quiet(BModels[[i]]  <- pbart(data[id == 1, which(var.type[1:i]!="S")], data[id == 1, (1 + i)], ndpost = Ndraws, nkeeptrain = 0, nkeeptest = 0))
+      quiet(BModels[[i]]  <- BART::pbart(data[id == 1, which(var.type[1:i]!="S")], data[id == 1, (1 + i)], ndpost = Ndraws, nkeeptrain = 0, nkeeptest = 0))
     } else if (continous[1+i] == TRUE & var.type[1 + i] != "Fi" & Suppress == TRUE) {
-      quiet(BModels[[i]]  <- wbart(data[id == 1, which(var.type[1:i] != "S")], data[id == 1, (1 + i)], ndpost = Ndraws, nkeeptrain = 0, nkeeptest = 0))
+      quiet(BModels[[i]]  <- BART::wbart(data[id == 1, which(var.type[1:i] != "S")], data[id == 1, (1 + i)], ndpost = Ndraws, nkeeptrain = 0, nkeeptest = 0))
     } else if (continous[1+i] == FALSE & var.type[1 + i] != "Fi" & Suppress == FALSE) {
-      BModels[[i]]  <- pbart(data[id == 1, which(var.type[1:i] != "S")], data[id == 1, (1 + i)], ndpost = Ndraws, nkeeptrain = 0, nkeeptest = 0)
+      BModels[[i]]  <- BART::pbart(data[id == 1, which(var.type[1:i] != "S")], data[id == 1, (1 + i)], ndpost = Ndraws, nkeeptrain = 0, nkeeptest = 0)
     } else if (continous[1+i] == TRUE & var.type[1 + i] != "Fi" & Suppress == FALSE) {
-      BModels[[i]]  <- wbart(data[id == 1, which(var.type[1:i] != "S")], data[id == 1, (1 + i)], ndpost = Ndraws, nkeeptrain = 0, nkeeptest = 0)
+      BModels[[i]]  <- BART::wbart(data[id == 1, which(var.type[1:i] != "S")], data[id == 1, (1 + i)], ndpost = Ndraws, nkeeptrain = 0, nkeeptest = 0)
     }
   }
 
-  y <- matrix(nrow = 2 * n_O, ncol = Ndraws)
+  y <- matrix(nrow = length(regimes) * n_O, ncol = Ndraws)
   ## MC-integration over "Ndraws" iterations
   for(it in 1:Ndraws) {
     k <- 1
@@ -45,7 +44,8 @@ GcompBART <- function(data,
       if(var.type[j] == "C") {
         x <- lapply(x, function(x2) {pred_comb(continous[j], BModels[[j - 1]], x2, it)})
       } else if(var.type[j] == "Fi") {
-        x <- list(cbind(x[[1]], exp.reg[l]), cbind(x[[length(x)]], cont.reg[l]))
+        x <- Map(cbind, x, regimes)
+        # list(cbind(x[[1]], exp.reg[l]), cbind(x[[length(x)]], cont.reg[l]))
         l <- l + 1
       } else if(var.type[j] == "S") {
         x <- lapply(x, function(x2) {pred_surv(BModels[[j - 1]], x2, it)})
@@ -54,20 +54,22 @@ GcompBART <- function(data,
           tmp <- lapply(x, function(x2) {pred_y(continous[j], BModels[[j - 1]], x2, it)})
           y[k, it] <- sapply(tmp, mean)
           if(continous[j] == FALSE) {
-            x[[1]] <- cbind(x[[1]], rbinom(length(tmp[[1]]), 1, prob = tmp[[1]]))
+            tmp <- lapply(tmp, function(tmp2) rbinom(length(tmp2), 1, prob = tmp2))
+            x <- Map(cbind, x, tmp)
           } else if(continous[j] == TRUE) {
-            x[[1]] <- cbind(x[[1]], rnorm(length(tmp[[1]]), mean = tmp[[1]], sd = mean(BModels[[j - 1]]$sigma)))
+            tmp <- lapply(tmp, function(tmp2) rnorm(length(tmp2), mean = tmp2, sd = mean(BModels[[j-1]]$sigma)))
+            x <- Map(cbind, x, tmp)
           }
         } else {
           tmp <- lapply(x, function(x2) {pred_y(continous[j], BModels[[j - 1]], x2, it)})
-          y[k:(k + 1), it] <- sapply(tmp, mean)
-          k <- k + 2
+          y[k:((k-1) + length(regimes)), it] <- sapply(tmp, mean)
+          k <- k + length(regimes)
           if(continous[j] == FALSE) {
-            x[[1]] <- cbind(x[[1]], rbinom(length(tmp[[1]]), 1, prob = tmp[[1]]))
-            x[[2]] <- cbind(x[[2]], rbinom(length(tmp[[2]]), 1, prob = tmp[[2]]))
+            tmp <- lapply(tmp, function(tmp2) rbinom(length(tmp2), 1, prob = tmp2))
+            x <- Map(cbind, x, tmp)
           } else if(continous[j] == TRUE) {
-            x[[1]] <- cbind(x[[1]], rnorm(length(tmp[[1]]), mean = tmp[[1]], sd = mean(BModels[[j-1]]$sigma)))
-            x[[2]] <- cbind(x[[2]], rnorm(length(tmp[[2]]), mean = tmp[[2]], sd = mean(BModels[[j-1]]$sigma)))
+            tmp <- lapply(tmp, function(tmp2) rnorm(length(tmp2), mean = tmp2, sd = mean(BModels[[j-1]]$sigma)))
+            x <- Map(cbind, x, tmp)
           }
         }
       }
@@ -76,10 +78,10 @@ GcompBART <- function(data,
       print(paste("done ", it, " (out of ", Ndraws,")", sep=""))
     }
   }
-  rownames(y) <- rep(c("Exp", "Contr"), n_O)
+  rownames(y) <- rep(1:length(regimes), n_O)
 
   means <- t(apply(y, 1, function(x) {c(mean(x), quantile(x, c(0.025, 0.975)))}))
-  rownames(means) <- rep(c("Exp", "Contr"), n_O)
+  rownames(means) <- paste0(rep(regimes, n_O), ":", rep(1:n_O, each=n_O))
   colnames(means) <- c("mu", "2.5%", "97.5%")
 
 
