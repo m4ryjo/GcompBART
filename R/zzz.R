c512f338060628650e63c0c90bdb806c9be485c6
Maria <maria.josefsson@umu.se@>
2025-11-24 13:49:26 +0100

    Initial commit

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+#' @importFrom MASS mvrnorm
+#' @importFrom stats ecdf na.omit pnorm qnorm quantile rbinom rmultinom rnorm runif sd
+#' @importFrom truncnorm rtruncnorm
+#' @importFrom EnvStats rtri
+#' @importFrom methods new
+#' @importFrom Rcpp evalCpp Module
+#' @importFrom rBeta2009 rdirichlet
+#'
+#' @useDynLib GcompBART, .registration = TRUE
+NULL
